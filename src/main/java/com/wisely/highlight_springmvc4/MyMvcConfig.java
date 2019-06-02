f8536063314ca05cc09d554fa1b05ebf5093615a
MrshaoZH <shaozehual@163.com>
2019-06-02 23:06:41 +0800

    静态资源映射

--- src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
+++ src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 //此处为配置的一个jsp的视图控制器
 @Configuration
-@EnableWebMvc
+@EnableWebMvc //开启springmvc支持，若无此句，重写WebMvcConfigurerAdapter无效
 @ComponentScan("com.wisely.highlight_springmvc4")
-public class MyMvcConfig {
+//继承WebMvcConfigurerAdapter，重写其方法可对springmvc进行配置
+public class MyMvcConfig extends WebMvcConfigurerAdapter{
 
 	@Bean
 	public InternalResourceViewResolver viewResolver(){
@@ -20,5 +23,11 @@ public class MyMvcConfig {
 		viewResolver.setSuffix(".jsp");
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//addResourceHandlers指的是文件放置的目录，addResourceHandler指的是对外暴露的访问路径
+		registry.addResourceHandler("/asserts/**").addResourceLocations("classpath:/assertss/");
 	}
 }
